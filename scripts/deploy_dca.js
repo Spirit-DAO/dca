926f65ec80e37c23f841b240c714c4a945dc96e7
Sifex <sifexdesign@gmail.com>
2024-05-09 17:57:47 +0200

    Deploy scripts

---
+++ scripts/deploy_dca.js
@@ -0,0 +1,30 @@
+const hre = require("hardhat");
+const fs = require('fs');
+const path = require('path');
+
+//address _proxy, address _automate, address _tresory, address _usdc
+
+async function main() {
+	const deployDataPath = path.resolve(__dirname, '../deploys.json');
+	let deploysData = JSON.parse(fs.readFileSync(deployDataPath, 'utf8'));
+
+	const deployExtraDataPath = path.resolve(__dirname, '../deploys_extra.json');
+	let deploysExtraData = JSON.parse(fs.readFileSync(deployExtraDataPath, 'utf8'));
+
+	const dcaFactory = await hre.ethers.getContractFactory("SpiritSwapDCA");
+	const dca = await dcaFactory.deploy(deploysExtraData.proxy, deploysExtraData.gelato, deploysExtraData.tresory, deploysExtraData.usdc);
+
+	await dca.waitForDeployment();
+
+	deploysData.dca = dca.target;
+	console.log(`deployed to ${dca.target}`);
+	
+	fs.writeFileSync(deployDataPath, JSON.stringify(deploysData), 'utf-8');
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
---
+++ scripts/verify.js
@@ -0,0 +1,31 @@
+const hre = require("hardhat");
+const fs = require('fs');
+const path = require('path');
+
+async function main() {
+
+    const deployDataPath = path.resolve(__dirname, '../deploys.json');
+    let deploysData = JSON.parse(fs.readFileSync(deployDataPath, 'utf8'));
+
+	const deployExtraDataPath = path.resolve(__dirname, '../deploys_extra.json');
+	let deploysExtraData = JSON.parse(fs.readFileSync(deployExtraDataPath, 'utf8'));
+
+	await hre.run("verify:verify", {
+		address: deploysData.dca,
+		constructorArguments: [
+			deploysExtraData.proxy,
+			deploysExtraData.gelato,
+			deploysExtraData.tresory,
+			deploysExtraData.usdc
+		],
+	});
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
